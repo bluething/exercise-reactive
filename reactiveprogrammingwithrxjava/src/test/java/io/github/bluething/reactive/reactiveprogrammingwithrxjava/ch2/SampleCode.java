c167543522c8ece1121baee8b3d1683dfae73efc
bluething <habib.gunadarma@gmail.com>
2021-09-15 15:57:35 +0700

    Capturing all notification using Observer<T>
    An instance of `Observable` does not emit any events until someone subscribe it

--- reactiveprogrammingwithrxjava/src/test/java/io/github/bluething/reactive/reactiveprogrammingwithrxjava/ch2/SampleCode.java
+++ reactiveprogrammingwithrxjava/src/test/java/io/github/bluething/reactive/reactiveprogrammingwithrxjava/ch2/SampleCode.java
@@ -1,6 +1,9 @@
 package io.github.bluething.reactive.reactiveprogrammingwithrxjava.ch2;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 import org.junit.jupiter.api.Test;
 
 public class SampleCode {
@@ -30,6 +33,35 @@ public class SampleCode {
                 (Throwable t) -> System.out.println(t.getMessage()),
                 () -> System.out.println("Completed")
         );
+    }
+
+    @Test
+    public void observableSubscribe4() {
+        Observable<Tweet> tweets = Observable.just(new Tweet("This is tweet"));
+
+        Observer<Tweet> observer = new Observer<Tweet>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Tweet tweet) {
+                System.out.println(tweet.getText());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Completed");
+            }
+        };
+
+
     }
 
     class Tweet {
