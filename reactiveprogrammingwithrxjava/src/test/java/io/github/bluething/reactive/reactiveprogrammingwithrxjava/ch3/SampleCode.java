6e6ea9b6fe770bde98c001db70741500b1d224d8
bluething <habib.gunadarma@gmail.com>
2021-09-24 13:56:40 +0700

    Map sample code

--- reactiveprogrammingwithrxjava/src/test/java/io/github/bluething/reactive/reactiveprogrammingwithrxjava/ch3/SampleCode.java
+++ reactiveprogrammingwithrxjava/src/test/java/io/github/bluething/reactive/reactiveprogrammingwithrxjava/ch3/SampleCode.java
@@ -13,4 +13,14 @@ public class SampleCode {
         stringWithPrefixE.subscribe(s -> System.out.println(s));
     }
 
+    @Test
+    public void mapOperator() {
+        Observable<String> formatedInt = Observable.just(8, 9, 10)
+                .filter(i -> i % 3 > 0)
+                .map(i -> "#" + i*10)
+                .filter(s -> s.length() < 4);
+
+        formatedInt.subscribe(s -> System.out.println(s));
+    }
+
 }
