bc6fb4468b25f3c7f0b3209514f1f41789a2e3f4
bluething <habib.gunadarma@gmail.com>
2021-10-05 10:29:34 +0700

    Add test

---
+++ techioplayground/src/test/java/io/github/bluething/reactive/techioplayground/Part04TransformTest.java
@@ -0,0 +1,10 @@
+package io.github.bluething.reactive.techioplayground;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Part04TransformTest {
+
+    Part04Transform workshop = new Part04Transform();
+    ReactiveRepository<User> repository = new ReactiveUserRepository();
+
+}
