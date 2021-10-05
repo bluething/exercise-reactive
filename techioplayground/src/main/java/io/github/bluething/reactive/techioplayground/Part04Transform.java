167dba5db55030502f50f27bdccd5698cb1df630
bluething <habib.gunadarma@gmail.com>
2021-10-05 10:49:16 +0700

    Test pass

---
+++ techioplayground/src/main/java/io/github/bluething/reactive/techioplayground/Part04Transform.java
@@ -0,0 +1,25 @@
+package io.github.bluething.reactive.techioplayground;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Part04Transform {
+    Mono<User> capitalizeOne(Mono<User> mono) {
+        return mono.map(user ->
+                new User(user.getUsername().toUpperCase(), user.getFirstname().toUpperCase(), user.getLastname().toUpperCase()));
+    }
+
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        return flux.map(user ->
+                new User(user.getUsername().toUpperCase(), user.getFirstname().toUpperCase(), user.getLastname().toUpperCase()));
+    }
+
+    Flux<User> asyncCapitalizeMany(Flux<User> flux) {
+        return flux.flatMap(user -> asyncCapitalizeUser(user));
+    }
+
+    Mono<User> asyncCapitalizeUser(User u) {
+        return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
+    }
+
+}
