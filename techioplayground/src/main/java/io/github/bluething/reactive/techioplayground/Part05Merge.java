e50ae8ac06c058ba076aaf4e7584377433af5f7c
bluething <habib.gunadarma@gmail.com>
2021-10-05 10:58:02 +0700

    Test pass

---
+++ techioplayground/src/main/java/io/github/bluething/reactive/techioplayground/Part05Merge.java
@@ -0,0 +1,18 @@
+package io.github.bluething.reactive.techioplayground;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Part05Merge {
+    Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
+        return flux1.mergeWith(flux2);
+    }
+
+    Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
+        return flux1.concatWith(flux2);
+    }
+
+    Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
+        return Flux.concat(mono1, mono2);
+    }
+}
